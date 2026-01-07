c4176249a08669dab5de858e52aa0c754de31c29
inceptorzy <inceptorzy@proton.me>
2026-01-07 16:48:22 +0100

    Ostateczna Czystka Kodu

--- script.js
+++ script.js
@@ -2,53 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const manifesty = [
         {
-            obraz: 'assets/images/manifest-1.png'
-            tytul: 'Twoja siła woli nie istnieje.',
-            tresc: `Mówią ci:
-
-'Weź się w garść.'
-'Miej więcej dyscypliny.'
-'Wszystko jest w twojej głowie.'
-
-Kłamią.
-
-Próbujesz kontrolować biochemiczny sztorm za pomocą myśli. To jak próba zatrzymania huraganu krzykiem.
-
-Twoje ciało nie słucha twojej woli, bo jest zaprogramowane przez wroga:
-Cukier. Stan zapalny. Bezruch.
-
-Chcesz odzyskać kontrolę?
-
-Przestań walczyć ze sobą.
-Zacznij walczyć z wrogiem, którego wpuściłeś do świątyni.`
+            obraz: 'https://i.imgur.com/Qk7cQUc.png',
+            tytul: 'Twoja otyłość nie jest chorobą. Jest zbroją.',
+            tresc: `Mówią ci, że to genetyka...` // Wklej tu pełne manifesty
         },
-        {
-            obraz: 'assets/images/manifest-7.png'
-            tytul: 'Nie jesteś uzależniony od jedzenia. Jesteś niewolnikiem potwora, którego sam karmisz.',
-            tresc: `Myślisz, że to 'ty' chcesz tego ciastka.
-Myślisz, że to 'twoja' ochota na chipsy.
-
-To iluzja.
-
-W tobie, w mrocznych, zapomnianych katakumbach twoich jelit i twojego mózgu, żyje bestia. Stworzyłeś ją z cukru, przetworzonego jedzenia i lat złych nawyków. Wyhodowałeś ją. Uczyniłeś ją silną.
-
-A teraz... to ona rządzi.
-
-To nie jest głód. To jest jej ryk.
-To nie jest zachcianka. To jest jej rozkaz.
-
-Gdy czujesz ten nieodparty impuls, by sięgnąć po truciznę, to nie jesteś ty. To jest ona, twój Pan, która domaga się swojej ofiary.
-
-A ty, posłuszny niewolnik, idziesz i ją karmisz. Z każdym kęsem, ona rośnie w siłę, a ty stajesz się słabszy.
-
-To jest idealny cykl niewolnictwa.
-
-Chcesz się wyzwolić? Musisz przestać z nią negocjować.
-Musisz ją zagłodzić.
-Musisz przetrwać jej agonalne krzyki.
-
-Bo tylko wtedy, gdy ona umrze, ty wreszcie odzyskasz swój tron.`
-        }
+        // ... więcej manifestów
     ];
 
     const galeria = document.querySelector('.galeria-sygnetow');
@@ -100,4 +58,4 @@ Bo tylko wtedy, gdy ona umrze, ty wreszcie odzyskasz swój tron.`
             zamknijModal();
         }
     });
-});
+});```
